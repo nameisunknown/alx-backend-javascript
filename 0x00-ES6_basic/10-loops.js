959b5b3aa6adbd0586e14cdd5d55b5f77184c6b8
nameisunknown <orangekapital@gmail.com>
2024-07-24 14:37:54 +0300

    updates --

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   const newArry = [];
   for (const idx of array) {
-    newArry.push(newArry + appendString);
+    newArry.push(appendString + idx);
   }
 
   return array;
