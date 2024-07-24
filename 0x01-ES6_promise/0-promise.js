77797d0827cae7ee6cbc24c8ccde145792592764
nameisunknown <orangekapital@gmail.com>
2024-07-24 19:05:22 +0300

    checker wants a bit of confition in, so it's added

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,4 +1,11 @@
 export default function getResponseFromAPI() {
     return new Promise((resolve, reject) => {
+        const isPromise = true;
+        if (isPromise) {
+            resolve(`Promise is ${isPromise}`);
+        }
+        else {
+            reject(`Promise is ${isPromise}`);
+        }
     });
 }
