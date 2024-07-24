6393df5e3c69b4377b0fec90d2553896252fd5db
nameisunknown <orangekapital@gmail.com>
2024-07-24 19:03:16 +0300

    updates on itt

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,4 @@
-export function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {});
+export default function getResponseFromAPI() {
+    return new Promise((resolve, reject) => {
+    });
 }
