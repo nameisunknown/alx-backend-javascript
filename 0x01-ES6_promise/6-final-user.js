1fa35f3ca752dfbe1128ffb4c5d429e44933ae6a
nameisunknown <orangekapital@gmail.com>
2024-07-25 15:14:22 +0300

    handling multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,14 +1,11 @@
-import signUpUser from "./4-user-promise";
-import uploadPhoto from "./5-photo-reject";
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]).then((results) =>
-    results.map((res) => ({
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]).then((result) => {
+    return result.map((res) => ({
       status: res.status,
-      value: res.status === "fulfilled" ? res.value : String(res.reason),
-    }))
-  );
+      value: res.status === 'fulfilled' ? res.value : String(res.reason),
+    }));
+  });
 }
