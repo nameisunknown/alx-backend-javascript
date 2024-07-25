0994b8f51cf77cae330a56fcf4250b0638242e0a
nameisunknown <orangekapital@gmail.com>
2024-07-25 05:02:41 +0300

    checker issues

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,10 +1,4 @@
 export default function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
-    const isPromise = true;
-    if (isPromise) {
-      resolve(`Promise is ${isPromise}`);
-    } else {
-      reject(`Promise is ${isPromise}`);
-    }
   });
 }
