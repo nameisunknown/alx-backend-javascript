9ef8fbb457d71d694f1f64d3c12f6a1a733c3de1
nameisunknown <orangekapital@gmail.com>
2024-07-25 16:40:11 +0300

    using async and await

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,14 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser(){
+  const asyncResObj = {photo: null, user: null};
+
+  try{
+    asyncResObj.photo = await uploadPhoto().then((res) => res);
+    asyncResObj.user = await createUser().then((res) => res);
+  } catch(err) {
+    return {photo: null, user: null};  
+  }
+
+  return asyncResObj;
+}
