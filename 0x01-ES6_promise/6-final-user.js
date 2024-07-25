4ddb6e6683477d53c9b3382d998003998efeb689
nameisunknown <orangekapital@gmail.com>
2024-07-25 14:59:13 +0300

    working on load balancer

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,8 +3,8 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => results.map((r) => ({
-      status: r.status,
-      value: r.status === 'fulfilled' ? r.value : String(r.reason),
+    .then((results) => results.map((res) => ({
+      status: res.status,
+      value: res.status === 'fulfilled' ? res.value : String(res.reason),
     })));
 }
