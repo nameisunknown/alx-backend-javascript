c4539a3ac03262eba4fd73ee3e4f740d448ae592
nameisunknown <orangekapital@gmail.com>
2024-07-25 16:10:12 +0300

    throwing an error

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,12 @@
+export default function guardrail(mathFunction){
+  const queue = [];
+  try{
+    const mathResult = mathFunction();
+    queue.push(mathResult);
+  } catch(err){
+    queue.push(`Error: ${err.message}`);
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+  return queue;
+}
