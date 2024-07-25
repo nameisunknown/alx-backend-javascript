a4a554dbbd1df0b08e23cdbac3e282206590facf
nameisunknown <orangekapital@gmail.com>
2024-07-25 06:42:44 +0300

    using Promise.all()

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,12 @@
+import { uploadPhoto, createUser } from './utils'; 
+
+export default function handleProfileSignup(){
+  return Promise.all([uploadPhoto(), createUser()])
+  .then((data) => {
+    console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`)
+    
+  })
+  .catch((err) => {
+    console.log('Signup system offline')
+  })
+}
