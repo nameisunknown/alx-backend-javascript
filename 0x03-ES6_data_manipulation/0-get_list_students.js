ee19cc701a98cf9556d2422c0e09aca869883dec
nameisunknown <orangekapital@gmail.com>
2024-07-31 01:33:41 +0300

    basic list of objects

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -4,20 +4,20 @@ export default function getListStudents() {
   const obj1 = {
     id: 1,
     firstName: 'Guillame',
-    location: 'San Francisco'
-  }
+    location: 'San Francisco',
+  };
 
   const obj2 = {
     id: 2,
     firstName: 'James',
-    location: 'Columbia'
-  }
+    location: 'Columbia',
+  };
 
   const obj3 = {
     id: 3,
     firstName: 'Serena',
-    location: 'San Francisco'
-  }
+    location: 'San Francisco',
+  };
 
   arrayOfObjects.push(obj1, obj2, obj3);
 
