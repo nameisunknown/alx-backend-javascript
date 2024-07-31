edec95db4979c5a54eff4ab848f3632d01be4c54
nameisunknown <orangekapital@gmail.com>
2024-07-31 15:52:58 +0300

    fixing checker issue

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,25 +1,19 @@
 export default function getListStudents() {
-  const arrayOfObjects = [];
-
-  const obj1 = {
-    id: 1,
-    firstName: 'Guillame',
-    location: 'San Francisco',
-  };
-
-  const obj2 = {
-    id: 2,
-    firstName: 'James',
-    location: 'Columbia',
-  };
-
-  const obj3 = {
-    id: 5,
-    firstName: 'Serena',
-    location: 'San Francisco',
-  };
-
-  arrayOfObjects.push(obj1, obj2, obj3);
-
-  return arrayOfObjects;
+  return [
+    {
+      id: 1,
+      firstName: 'Guillame',
+      location: 'San Francisco',
+    },
+    {
+      id: 2,
+      firstName: 'James',
+      location: 'Columbia',
+    },
+    {
+      id: 5,
+      firstName: 'Serena',
+      location: 'San Francisco',
+    },
+  ];
 }
